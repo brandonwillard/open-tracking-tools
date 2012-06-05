b4760bee171bb831e2f8f1845f8728a573368796
Brandon Willard <brandonwillard@gmail.com>
2012-06-05 12:17:53 -0500

    - added missing classes

---
+++ src/main/java/org/openplans/tools/tracking/impl/FilterInformation.java
@@ -0,0 +1,27 @@
+package org.openplans.tools.tracking.impl;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import com.google.common.base.Objects;
+
+public class FilterInformation {
+
+  private final InferredPath path;
+  private final Collection<InferredPathEntry> evaluatedPaths;
+  
+  @SuppressWarnings("unchecked")
+  public FilterInformation(InferredPath path, Collection<InferredPathEntry> evaluatedPaths) {
+    this.path = path;
+    this.evaluatedPaths = (Collection<InferredPathEntry>) Objects.firstNonNull(evaluatedPaths, Collections.emptyList());
+  }
+
+  public InferredPath getPath() {
+    return path;
+  }
+
+  public Collection<InferredPathEntry> getEvaluatedPaths() {
+    return evaluatedPaths;
+  }
+
+}
---
+++ src/main/java/org/openplans/tools/tracking/impl/statistics/StatisticsUtil.java
@@ -0,0 +1,55 @@
+package org.openplans.tools.tracking.impl.statistics;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.openplans.tools.tracking.impl.InferredPathEntry;
+
+import gov.sandia.cognition.math.LogMath;
+import gov.sandia.cognition.statistics.DataDistribution;
+import gov.sandia.cognition.statistics.distribution.DefaultDataDistribution;
+import gov.sandia.cognition.util.DefaultWeightedValue;
+
+public class StatisticsUtil {
+  
+  public static <DistributionType, SupportType> DataDistribution<SupportType> getLogNormalizedDistribution(
+      Map<SupportType, DefaultWeightedValue<DistributionType>> map) {
+    final DataDistribution<SupportType> result = new DefaultDataDistribution<SupportType>();
+
+    /*-
+     * Normalize to avoid zero probs.
+     */
+    double totalLikelihood = Double.NEGATIVE_INFINITY;
+    for (final DefaultWeightedValue<DistributionType> weight : map.values()) {
+      totalLikelihood = LogMath.add(weight.getWeight(), totalLikelihood);
+    }
+    
+    for (final Entry<SupportType, DefaultWeightedValue<DistributionType>> entry : map.entrySet()) {
+      final double weight = entry.getValue().getWeight() - totalLikelihood;
+      result.set(entry.getKey(), Math.exp(weight));
+    }
+    
+    return result;
+  }
+
+  public static <SupportType> DataDistribution<SupportType> getLogNormalizedDistribution(
+    Collection<DefaultWeightedValue<SupportType>> collection) {
+    final DataDistribution<SupportType> result = new DefaultDataDistribution<SupportType>();
+
+    /*-
+     * Normalize to avoid zero probs.
+     */
+    double totalLikelihood = Double.NEGATIVE_INFINITY;
+    for (final DefaultWeightedValue<SupportType> weight : collection) {
+      totalLikelihood = LogMath.add(weight.getWeight(), totalLikelihood);
+    }
+    
+    for (final DefaultWeightedValue<SupportType> entry : collection) {
+      final double weight = entry.getWeight() - totalLikelihood;
+      result.set(entry.getValue(), Math.exp(weight));
+    }
+    
+    return result;
+  }
+}
