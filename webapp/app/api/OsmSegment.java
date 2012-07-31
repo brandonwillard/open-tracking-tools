e535ac51ec3930c878b57d49963c4b081b66d98f
novalis <novalis@openplans.org>
2012-07-31 13:44:43 -0400

    add cordinate offsetting for line segments

--- webapp/app/api/OsmSegment.java
+++ webapp/app/api/OsmSegment.java
@@ -5,10 +5,14 @@ import org.codehaus.jackson.map.annotate.JsonSerialize;
 import utils.GeoJSONSerializer;
 
 import com.vividsolutions.jts.algorithm.Angle;
+import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Geometry;
+import com.vividsolutions.jts.geom.GeometryFactory;
 
 public class OsmSegment {
-
+  private final static double OFFSET = 8;
+  private static GeometryFactory gf = new GeometryFactory();
+    
   private final int id;
   private final Geometry geom;
   private final Double angle;
@@ -29,11 +33,35 @@ public class OsmSegment {
       this.length = null;
     }
     this.id = i;
-    this.geom = g;
+    this.geom = offset(g);
     this.name = name;
   }
 
-  @JsonSerialize
+  private Geometry offset(Geometry g) {
+      if (g == null) return null;
+      Coordinate[] oldCoords = g.getCoordinates();
+      int nCoords = oldCoords.length;
+      Coordinate[] newCoords = new Coordinate[nCoords];
+      
+      for (int i = 0; i < nCoords - 1; ++i) {
+          Coordinate coord0 = oldCoords[i];
+          Coordinate coord1 = oldCoords[i+1];
+          
+          double dx = coord1.x - coord0.x;
+          double dy = coord1.y - coord0.y;
+          
+          double length = Math.sqrt(dx * dx + dy * dy);
+          
+          Coordinate c0 = new Coordinate(coord0.x - OFFSET * dy / length, coord0.y - OFFSET * dx / length);
+          Coordinate c1 = new Coordinate(coord1.x - OFFSET * dy / length, coord1.y - OFFSET * dx / length);
+          newCoords[i] = c0;
+          newCoords[i+1] = c1; //will get overwritten except at last iteration
+      }
+      
+      return gf.createLineString(newCoords);
+  }
+
+@JsonSerialize
   public Double getAngle() {
     return angle;
   }
